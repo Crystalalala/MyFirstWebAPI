b51075534e40bc29376be5b26789f4b25886bae5
Jestoni Cruz <jestoni.estrada.cruz@gmail.com>
2023-03-13 00:56:19 +0800

    Data Structure Controller

---
+++ MyFirstWebAPI/DataStructureController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebAPI
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataStructureController : ControllerBase
+    {
+        private readonly ILogger<DataStructureController> _logger;
+
+        public DataStructureController(ILogger<DataStructureController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
