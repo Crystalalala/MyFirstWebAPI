c317043c85e112baf46027c4da284db19879a382
Jestoni Cruz <jestoni.estrada.cruz@gmail.com>
2023-03-13 01:17:59 +0800

    Add Stack Example

--- MyFirstWebAPI/Controllers/DataStructureController.cs
+++ MyFirstWebAPI/Controllers/DataStructureController.cs
@@ -18,5 +18,18 @@ namespace MyFirstWebAPI
             _logger = logger;
         }
 
+        [HttpGet("stack/number")]
+        public IActionResult ExampleOFStack()
+        {
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+            numbers.Push(4);
+            numbers.Push(5);
+
+            foreach (var item in numbers) ;
+            return Ok(numbers);
+        }
     }
 }
