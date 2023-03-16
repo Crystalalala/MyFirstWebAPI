3c7dba2997d16a89369022f82214f465034ce8af
unknown <CystalJoy.Lomio@gmail.com>
2023-03-17 00:51:06 +0800

    Additional For List

--- MyFirstWebAPI/Controllers/ListsController.cs
+++ MyFirstWebAPI/Controllers/ListsController.cs
@@ -60,5 +60,188 @@ namespace MyFirstWebAPI.Controllers
 
             return Ok(result);
         }
+
+
+        [HttpGet("school")]
+        public IActionResult GetSchool([FromQuery] bool isPrivate)
+        {
+            List<School> schools = new List<School>(){
+                new School(){ Id = 1, 
+                    Name="JRU",
+                    Address="Manda",
+                    BuildingNo = 1,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                new School(){ Id = 2,
+                    Name="SBU",
+                    Address="Manila",
+                    BuildingNo = 2,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                    new School(){ Id = 3,
+                    Name="SEHS",
+                    Address="Marikina",
+                    BuildingNo = 3,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+                     new School(){ Id = 4,
+                    Name="RTU",
+                    Address="Manda",
+                    BuildingNo = 4,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+            };
+
+
+            string result = "";
+
+            schools = schools.Where(x => x.IsPrivate == isPrivate).ToList();
+
+            schools.ForEach(x =>
+            {
+                result = result + $"School {x.Name} \n";
+            });
+
+            return Ok(result);
+        }
+
+        [HttpGet("school/one")]
+        public IActionResult GetSchoolOne([FromQuery] bool isPrivate)
+        {
+            List<School> schools = new List<School>(){
+                new School(){ Id = 1,
+                    Name="JRU",
+                    Address="Manda",
+                    BuildingNo = 1,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                new School(){ Id = 2,
+                    Name="SBU",
+                    Address="Manila",
+                    BuildingNo = 2,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                    new School(){ Id = 3,
+                    Name="SEHS",
+                    Address="Marikina",
+                    BuildingNo = 3,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+                     new School(){ Id = 4,
+                    Name="RTU",
+                    Address="Manda",
+                    BuildingNo = 4,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+            };
+
+
+            //School school = schools.Where(x => x.IsPrivate == isPrivate).FirstOrDefault();
+            School school = schools.Where(x => x.IsPrivate == isPrivate).LastOrDefault();
+
+            return Ok($"School {school.Name} \n");
+        }
+
+        [HttpGet("school/sort/asc")]
+        public IActionResult GetSchoolOrderByName()
+        {
+            List<School> schools = new List<School>(){
+                new School(){ Id = 1,
+                    Name="JRU",
+                    Address="Manda",
+                    BuildingNo = 1,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                new School(){ Id = 2,
+                    Name="SBU",
+                    Address="Manila",
+                    BuildingNo = 2,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                    new School(){ Id = 3,
+                    Name="SEHS",
+                    Address="Marikina",
+                    BuildingNo = 3,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+                     new School(){ Id = 4,
+                    Name="RTU",
+                    Address="Manda",
+                    BuildingNo = 4,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+            };
+
+
+            string result = "";
+
+            schools = schools.OrderBy(x => x.Name).ToList();
+
+            schools.ForEach(x =>
+            {
+                result = result + $"School {x.Name} \n";
+            });
+
+            return Ok(result);
+        }
+
+        [HttpGet("school/sort/desc")]
+        public IActionResult GetSchoolOrderByNameDesc()
+        {
+            List<School> schools = new List<School>(){
+                new School(){ Id = 1,
+                    Name="JRU",
+                    Address="Manda",
+                    BuildingNo = 1,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                new School(){ Id = 2,
+                    Name="SBU",
+                    Address="Manila",
+                    BuildingNo = 2,
+                    Country = "PH",
+                    IsPrivate = true
+                },
+                    new School(){ Id = 3,
+                    Name="SEHS",
+                    Address="Marikina",
+                    BuildingNo = 3,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+                     new School(){ Id = 4,
+                    Name="RTU",
+                    Address="Manda",
+                    BuildingNo = 4,
+                    Country = "PH",
+                    IsPrivate = false
+                },
+            };
+
+
+            string result = "";
+
+            schools = schools.OrderByDescending(x => x.Name).ToList();
+
+            schools.ForEach(x =>
+            {
+                result = result + $"School {x.Name} \n";
+            });
+
+            return Ok(result);
+        }
     }
 }
